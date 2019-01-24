b06c82e03f7ae9c5af47054e56210fa28455e659
Krzysztof Korbacz <k.korbacz@gmail.com>
2019-01-24 19:42:16 +0100

    Add some more samples to extensions

--- src/main/kotlin/org/kkorbacz/features/Extensions.kt
+++ src/main/kotlin/org/kkorbacz/features/Extensions.kt
@@ -22,7 +22,37 @@ fun main(args: Array<String>) {
         append("ef")
     }
 
+    fun buildString(builderAction: StringBuilder.() -> Unit): String =
+            StringBuilder().apply(builderAction).toString()
+
+    // [20, 24, 28]
+    listOf(5, 6, 7) * 4
 }
 
-fun buildString(builderAction: StringBuilder.() -> Unit): String =
-        StringBuilder().apply(builderAction).toString()
+// can be called on an object variable even if its value is null
+fun Any?.toString(): String {
+    if (this == null) return "null"
+    return toString()
+}
+
+operator fun List<Int>.times(by: Int): List<Int> {
+    return this.map { it * by }
+}
+
+class Column<T>
+
+// member extensions can't be called outside of their applicability scope
+open class Table {
+    fun <T> Column<T>.primaryKey(): Column<T> = Column()
+    // restricting receiver type
+    fun Column<Int>.autoIncrement(): Column<Int> = Column()
+
+    fun integer(name: String): Column<Int> = Column()
+    fun varchar(name: String, length: Int): Column<String> = Column()
+
+}
+
+object Country : Table() {
+    val id = integer("id").autoIncrement().primaryKey()
+    val name = varchar("name", 50)
+}
