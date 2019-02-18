c62d8b4f7c55f87d38739726dbb6f30e946dad92
Krzysztof Korbacz <k.korbacz@gmail.com>
2019-02-18 20:38:39 +0100

    Improve Nullability examples

--- src/main/kotlin/org/kkorbacz/features/Nullability.kt
+++ src/main/kotlin/org/kkorbacz/features/Nullability.kt
@@ -4,16 +4,17 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 fun main(args: Array<String>) {
-    // defaultString
+    val nonNullableVariable: String = "hello"
+    println(nonNullableVariable) // hello
+
     val nullString: String? = null
     val result = nullString?.toUpperCase()?.trim() ?: "defaultString"
-    println(result)
+    println(result) // defaultString
 
     fun maybeDateAsString(): String? = "2019-01-01"
 
-    // 2019-01-01
     val dateAsString = maybeDateAsString()?.let {
         LocalDate.parse(it, DateTimeFormatter.ISO_LOCAL_DATE)
     } ?: LocalDate.now()
-    println(dateAsString)
+    println(dateAsString) // 2019-01-01
 }
